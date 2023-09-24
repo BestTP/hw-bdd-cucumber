9a03f9b00b5c57a492614250d509f8c563042f9b
Tanachat <s6401012630051@email.kmutnb.ac.th>
2023-09-25 01:47:57 +0700

    Finish

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,8 +13,21 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-    when /^the movies page$/ then '/movies'
+    when /^the home\s?page$/
+      '/'
+
+    when /^the RottenPotatoes home page/
+      '/movies'
+
+    when /^the Create New Movie page/
+      '/movies/new'
+
+    when /^Save Changes/
+      '/movies'
+
+    when /^Men In Black/
+      '/movies'
+    
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
